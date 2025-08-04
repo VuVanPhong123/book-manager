10eeba4c9a232c040924f4afd29cbf21e4ce15a1
unknown <phongbeo124@gmail.com>
2025-08-04 17:51:29 +0700

    update navbar + grid bookcard

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,17 +4,22 @@ import "./Navbar.css"
 const Navbar = () => {
   return (
     <nav className="navBar">
-      <Link className="navButton" to="/">
-        Home
-      </Link>
-      <Link className="navButton" to="/books">
-        Booklist
-      </Link>
-      <Link className='navButton' to = "/bookFind">
-        Find Book
-      </Link>
+      <button className="navButton">
+        <Link to="/home">Home</Link>
+      </button>
+      <button className="navButton">
+        <Link to="/books">Book list</Link>
+      </button>
+      <button className="navButton">
+        <Link to="/bookFind">Find Book</Link>
+      </button>
+      <button className="navButton">
+        <Link to="/bookManage">Book Manager</Link>
+      </button>
+      <button className="navButton">
+        <Link to="/logIn">Account</Link>
+      </button>
     </nav>
   );
 };
-
 export default Navbar;
